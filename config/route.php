4ff591ce18cfb312887a7c65e8a9b9230fe9f775
Romss1 <romanberth@gmail.com>
2021-07-07 15:32:36 +0200

    routing

---
+++ config/route.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "/" => [
+        "controller" => "home.php", 
+        "action" => "home"
+    ]
+];
--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
 include '../vendor/autoload.php';
 use LifeStyleCoding\Container\Container;
+
+$url = filter_input(INPUT_SERVER, "PATH_INFO");
+var_dump($url);
+$table = require ('../config/route.php');
+
+if($url === null) {
+    $url = "/";
+}
+
+foreach ($table as $urlRoad => $road):
+
+    if($url === $urlRoad) {
+        include './../src/Controller/'.$road['controller'];
+        $road["action"]();
+    }
+
+endforeach;
+
+$class = "\\App\\Controller\\Home";
+$method = "index";
+$container = new Container();
+$instance = $container->resolve($class);
+$container->execute($instance, $method);
